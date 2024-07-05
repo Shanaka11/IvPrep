3de5f18964d8eaa8e712a40679b4c8967d4de3ea
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2024-07-05 13:58:07 +0530

    Fix format issues

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,7 +1,7 @@
+import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
-import dotenv from "dotenv";
 
 dotenv.config();
 
