a77669eaa5486588e9b71e39cbbeee8172f40a96
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2024-07-10 13:16:48 +0530

    Added get random questions service, usecase

---
+++ src/questions/services/utils/formatFullQuestionDrizzle.ts
@@ -0,0 +1,46 @@
+import { ReadQuestionDto } from "@/questions/models/question";
+import { ReadTopicDto } from "@/questions/models/topic";
+
+type ResultType = {
+  topic: ReadTopicDto;
+  question: ReadQuestionDto;
+  question_topic: {
+    questionId: ReadQuestionDto["id"];
+    topicId: ReadTopicDto["id"];
+  };
+};
+
+export const formatFullQuestionDrizzle = (results: ResultType[]) => {
+  const questionsWithTopics: {
+    [key: number]: {
+      id: number;
+      question: string;
+      authorId: string;
+      active: boolean;
+      createdAt: Date;
+      updatedAt: Date;
+      topics: { id: number; name: string }[];
+    };
+  } = {};
+
+  results.forEach((result) => {
+    if (!questionsWithTopics[result.question.id]) {
+      questionsWithTopics[result.question.id] = {
+        id: result.question.id,
+        question: result.question.question,
+        authorId: result.question.authorId,
+        active: result.question.active,
+        createdAt: result.question.createdAt,
+        updatedAt: result.question.updatedAt,
+        topics: [],
+      };
+    }
+
+    questionsWithTopics[result.question.id].topics.push({
+      id: result.topic.id,
+      name: result.topic.name,
+    });
+  });
+
+  return Object.values(questionsWithTopics);
+};
