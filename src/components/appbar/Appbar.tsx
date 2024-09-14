4f72f3e609f2856cc0101be3b7164182b74362a1
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2024-09-14 21:54:23 +0530

    feat(IP-22): Appbar styled (#13)
    * Created initial Light theme colours
    
    * Setup Darkmode and the mode toggle
    
    * Finished styling the app bar

--- src/components/appbar/Appbar.tsx
+++ src/components/appbar/Appbar.tsx
@@ -2,23 +2,26 @@ import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
+import { ModeToggle } from "../modeToggle/ModeToggle";
 import { Button } from "../ui/button";
+import AppLogo from "./AppLogo";
 import UserMenu from "./UserMenu";
 
 const Appbar = () => {
   return (
-    <div className="bg-blue-700 flex justify-between items-center px-4">
-      <Link className="w-full text-xl" href="/">
-        IvPrep
-      </Link>
-      <SignedOut>
-        <SignInButton>
-          <Button className="rounded-3xl w-24">Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserMenu />
-      </SignedIn>
+    <div className="flex justify-between items-center px-5">
+      <AppLogo />
+      <div className="flex gap-2">
+        <SignedOut>
+          <SignInButton>
+            <Button className="rounded-3xl w-24">Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserMenu />
+        </SignedIn>
+        <ModeToggle />
+      </div>
     </div>
   );
 };
