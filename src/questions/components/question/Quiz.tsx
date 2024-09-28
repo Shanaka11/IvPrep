bb31af8109f2386cf1f7869f5fd50d7331daf801
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2024-09-28 18:11:07 +0530

    Styled Question Card

--- src/questions/components/question/Quiz.tsx
+++ src/questions/components/question/Quiz.tsx
@@ -1,3 +1,11 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
 import { ReadQuestionDto } from "@/questions/models/question";
 import React from "react";
@@ -11,16 +19,14 @@ const Quiz = ({ questions }: QuizProps) => {
     <div className="flex gap-4 flex-col flex-wrap mt-5">
       {questions.map((question, index) => {
         return (
-          <div
-            className="p-5 border rounded-xl min-h-14 grow flex flex-col gap-2"
-            key={question.id}
-          >
-            <div className="text-sm text-primary w-full">
-              Question {index + 1}
-            </div>
-            <div className="w-full">{question.question}</div>
-            <Textarea className="w-full mt-2" placeholder="Answer" rows={3} />
-          </div>
+          <Card key={question.id}>
+            <CardHeader>
+              <CardTitle>{question.question}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Textarea className="w-full mt-2" placeholder="Answer" rows={3} />
+            </CardContent>
+          </Card>
         );
       })}
     </div>
